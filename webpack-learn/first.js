3cd4ddc5eac5b1fc8cb15ea50c5b2634ea11860d
xinliya <xinliya2014@163.com>
2017-07-14 10:32:12 +0800

    webpackInstall

---
+++ webpack-learn/first.js
@@ -0,0 +1,6 @@
+require('./second.js');  //引入js
+require('./style.css'); //引入css
+function first(str){
+		alert(str);
+}
+first('第一页第一页!!!!!!!!!');
---
+++ webpack-learn/second.js
@@ -0,0 +1,5 @@
+function second(){
+	 return {
+		 
+	 }
+}
